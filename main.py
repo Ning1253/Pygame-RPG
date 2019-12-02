922debd813c3387dce278c283f3aab368618a5a3
Ning1253 <alastmann9@gmail.com>
2019-12-02 17:30:41 +0000

    Created main.py
    This contains the display, settings and main loop of the game. To be the base file from which to launch tests, until game is compiled.

---
+++ main.py
@@ -0,0 +1,29 @@
+# The main file, containing the display and the main loop of the game.
+import pygame
+
+# Initialise Pygame
+pygame.init()
+
+# Main Settings
+FPS = 60
+screen_size = (480, 480)
+clock = pygame.time.Clock()
+
+# Display Setup
+display = pygame.display.set_mode(screen_size)
+
+# Main Groups Setup
+all_sprites = pygame.sprite.Group()
+
+# Main Loop
+while True:
+    clock.tick(FPS) # Wait for next frame time
+    for event in pygame.event.get(): # Get User Inputs
+        if event.type == pygame.QUIT: # If the window is closed...
+            pygame.quit() # ...Close the Window
+    
+    # Display Refreshing
+    display.fill([0, 0, 0]) # Empty Screen
+    all_sprites.draw(display) # Render Sprites
+    
+    pygame.display.flip() # Show next frame
