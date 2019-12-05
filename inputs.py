3cb1e5e4e41b110973531f6529ad7048696e0cb6
Ning1253 <43830086+Ning1253@users.noreply.github.com>
2019-12-05 13:25:55 +0000

    changed main, added inputs file
    So that seperate files can get the frames inputs on update.

---
+++ inputs.py
@@ -0,0 +1,7 @@
+# Inputs for frame
+
+global inputs
+
+def grab_inputs():
+    global inputs
+    inputs = pygame.event.get() # To be run every frame
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # The main file, containing the display and the main loop of the game.
-import pygame
+import pygame, inputs
 
 def loop(running, clock, display, FPS, all_sprites):# Main Loop
     while running:
         clock.tick(FPS) # Wait for next frame time
-        for event in pygame.event.get(): # Get User Inputs
+        inputs.grab_inputs() # Get User Inputs
+        for event in inputs.inputs:
             if event.type == pygame.QUIT: # If the window is closed...
                 pygame.quit() # ...Close the Window
     
