7da03f732af29e344cac87bfc5f63d4acd43e4b1
Ning1253 <alastmann9@gmail.com>
2020-01-15 15:18:34 +0000

    Changed player to collide with walls Added directional collisions to functions

--- Classes/player.py
+++ Classes/player.py
@@ -1,6 +1,8 @@
 # File to contain Player and Inventory Classes
 import pygame
 import get_inputs as list_inputs
+from Functions import functions
+import current_rendered
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -11,7 +13,8 @@ class Player(pygame.sprite.Sprite):
     
     def update(self): # Update function, to be run every frame
         self.get_inputs()
-
+        self.wall_collision()
+        
     def get_inputs(self):
         if list_inputs.keys[pygame.K_w]:
             self.rect.y -= 1
@@ -21,3 +24,13 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 1
         if list_inputs.keys[pygame.K_d]:
             self.rect.x += 1
+    def wall_collision(self):
+        for wall in current_rendered.current_room.walls:
+            if functions.direction_collision(self, wall) == "Up Collision":
+                self.rect.y += 1
+            if functions.direction_collision(self, wall) == "Down Collision":
+                self.rect.y -= 1
+            if functions.direction_collision(self, wall) == "Left Collision":
+                self.rect.x += 1
+            if functions.direction_collision(self, wall) == "Right Collision":
+                self.rect.x -= 1
