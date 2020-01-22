8ceb031635bde749db5561ce1f1b984c77bbdc4e
Ning1253 <alastmann9@gmail.com>
2020-01-22 17:07:32 +0000

    Added player to test

--- Functions/functions.py
+++ Functions/functions.py
@@ -15,3 +15,12 @@ def direction_collision(spr1, spr2):
         return "Left Collision"
     if spr1.rect.right == spr2.rect.left + 1 and spr1.rect.top < spr2.rect.bottom - 1 and spr1.rect.bottom > spr2.rect.top + 1:
         return "Right Collision"
+
+def change_room(room):
+    current_rendered.all_sprites.empty()
+    current_rendered.current_room = room
+    
+    for wall in room.walls:
+        current_rendered.all_sprites.add(wall)
+    
+    current_rendered.all_sprites.add(current_rendered.current_player)
